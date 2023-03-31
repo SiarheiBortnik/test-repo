5f2085112f467ddf0d5975e7ef2c80dd10374dc3
SiarheiBortnik <61515826+SiarheiBortnik@users.noreply.github.com>
2023-03-31 16:46:14 +0300

    Update server.js

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const app = express();
 app.get('/', (req, res) => {
   res.send('Hello World!');
     
-const port = 3000;
+const port = 80;
 
 app.listen(port, () => {  
   console.log(`Server listening on port ${port}.`);  
