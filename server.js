84ee39c70f77591499c65014b5a01ca2c0246cbe
SiarheiBortnik <61515826+SiarheiBortnik@users.noreply.github.com>
2023-03-31 16:21:09 +0300

    Update server.js

--- server.js
+++ server.js
@@ -1,11 +1,10 @@
-var http = require('http'); // 1 - Import Node.js core module
+const express = require('express');  
+const app = express(); 
+app.get('/', (req, res) => {
+  res.send('Hello World!');
+    
+const port = 3000;
 
-var server = http.createServer(function (req, res) {   // 2 - creating server
-
-    //handle incomming requests here..
-
-});
-
-server.listen(5000); //3 - listen for any incoming requests
-
-console.log('Node.js web server at port 5000 is running..')
+app.listen(port, () => {  
+  console.log(`Server listening on port ${port}.`);  
+});  
