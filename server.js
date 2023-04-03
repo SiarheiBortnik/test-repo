fa43e148927dc808aee2c9c39ffdfc6274424b20
SiarheiBortnik <61515826+SiarheiBortnik@users.noreply.github.com>
2023-04-03 10:36:20 +0300

    Update server.js

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require('express');  
 const app = express(); 
 app.get('/', (req, res) => {
-  res.send('Hello World!');
+  res.send('<h1>Hello World!</h1>');
 });
 const port = 8080;
 
